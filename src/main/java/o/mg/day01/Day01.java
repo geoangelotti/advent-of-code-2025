729fd7a2fabed4718a6741e95e00aa8c5a7ae702
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-06 22:57:55 +0100

    feat: Day 01 Part 1 solution

--- src/main/java/o/mg/day01/Day01.java
+++ src/main/java/o/mg/day01/Day01.java
@@ -1,8 +1,38 @@
 package o.mg.day01;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day01 {
+    private static final PrintStream IO = System.out;
+
+    private static List<Pair<Character, Integer>> parseInput(String input) {
+        String[] lines = input.split("\n");
+        List<Pair<Character, Integer>> commands = new ArrayList<>();
+        for (String line : lines) {
+            char direction = line.charAt(0);
+            int rotation = Integer.parseInt(line.substring(1));
+            commands.add(new Pair<>(direction, rotation));
+        }
+        return commands;
+    }
 
     public static Integer processPart1(String input) {
-        return null;
+        var parsed = parseInput(input);
+        int position = 50;
+        int password = 0;
+        for (var command : parsed) {
+            if (command.first() == 'R') {
+                position += command.second();
+            } else if (command.first() == 'L') {
+                position -= command.second();
+            }
+            position = ((position % 100) + 100) % 100;
+            if (position == 0) {
+                password += 1;
+            }
+        }
+        return password;
     }
 }
