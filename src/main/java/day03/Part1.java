f2c34a68c857b88070a4eb228fb7612359669d51
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-10 00:49:51 +0100

    feat: day 3 part 1 skeleton
    - package
    - main
    - class
    - test

---
+++ src/main/java/day03/Part1.java
@@ -0,0 +1,13 @@
+package day03;
+
+import shared.Solver;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Part1 {
+    public static void main(String[] args) throws IOException {
+        Solver.processDay("day02.txt", Day03::processPart1);
+    }
+}
---
+++ src/test/java/day03/Day03Test.java
@@ -0,0 +1,23 @@
+package day03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day03Test {
+
+    private static final String INPUT = """
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+""";
+
+    @Test
+    void testProcessPart1() {
+        Integer result = Day03.processPart1(INPUT);
+        assertEquals(Integer.valueOf(357), result);
+    }
+
+}
+
