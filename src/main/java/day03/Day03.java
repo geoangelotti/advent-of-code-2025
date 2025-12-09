5c7856f2713e8555bb53beb2a0aab4dd81398aca
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-10 00:53:10 +0100

    feat: day 3 part 1 solution

--- src/main/java/day03/Day03.java
+++ src/main/java/day03/Day03.java
@@ -1,7 +1,34 @@
 package day03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Day03 {
+
+    private static List<String> parseInput(String input) {
+        return Arrays.asList(input.split("\n"));
+    }
+
     public static Integer processPart1(String input) {
-        return null;
+        var lines = parseInput(input);
+        var batteries = new ArrayList<Integer>();
+        for (String line : lines) {
+            var digits = line.split("");
+            int largest = Integer.parseInt(digits[0] + digits[1]);
+            int largestDigit = Math.max(Integer.parseInt(digits[0]), Integer.parseInt(digits[1]));
+            for (int i = 2; i < digits.length; i++) {
+                int currentDigit = Integer.parseInt(digits[i]);
+                int value = largestDigit * 10 + currentDigit;
+                if (value > largest) {
+                    largest = value;
+                }
+                if (currentDigit > largestDigit) {
+                    largestDigit = currentDigit;
+                }
+            }
+            batteries.add(largest);
+        }
+        return batteries.stream().reduce(0, Integer::sum);
     }
 }
