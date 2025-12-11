1b6d206b5c3599b99b0425b8e4da9b29fcbb1cc3
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-11 23:52:42 +0100

    feat: day 4 part 1 solution

--- src/main/java/day04/Day04.java
+++ src/main/java/day04/Day04.java
@@ -1,7 +1,55 @@
 package day04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day04 {
+    private static List<List<Character>> parseInput(String input) {
+        var lines = input.lines().toList();
+        var grid = new ArrayList<List<Character>>();
+        for (var line : lines) {
+            var row = new ArrayList<Character>();
+            for (var ch : line.toCharArray()) {
+                row.add(ch);
+            }
+            grid.add(row);
+        }
+        return grid;
+    }
+
+    private static int adjacentRolls(List<List<Character>> grid, int r, int c) {
+        int count = 0;
+        count += get(grid, r - 1, c - 1);
+        count += get(grid, r - 1, c);
+        count += get(grid, r - 1, c + 1);
+        count += get(grid, r, c - 1);
+        count += get(grid, r, c + 1);
+        count += get(grid, r + 1, c - 1);
+        count += get(grid, r + 1, c);
+        count += get(grid, r + 1, c + 1);
+        return count;
+    }
+
+    private static int get(List<List<Character>> grid, int r, int c) {
+        try {
+            return grid.get(r).get(c) == '@' ? 1 : 0;
+        } catch (IndexOutOfBoundsException ex) {
+            return 0;
+        }
+    }
+
     public static Integer processPart1(String input) {
-        return null;
+        var grid = parseInput(input);
+        int rows = grid.size();
+        int cols = grid.get(0).size();
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid.get(r).get(c) == '@') {
+                    count += adjacentRolls(grid, r, c) < 4 ? 1 : 0;
+                };
+            }
+        }
+        return count;
     }
 }
