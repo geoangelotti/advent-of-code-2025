d45f6fc87c294044c510f9b1a45717ecc08cab07
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-11 23:23:40 +0100

    feat: day 4 part 1 skeleton
    - class
    - test
    - package
    - main

---
+++ src/main/java/day04/Day04.java
@@ -0,0 +1,7 @@
+package day04;
+
+public class Day04 {
+    public static Integer processPart1(String input) {
+        return null;
+    }
+}
---
+++ src/main/java/day04/Part1.java
@@ -0,0 +1,11 @@
+package day04;
+
+import shared.Solver;
+
+import java.io.IOException;
+
+public class Part1 {
+    public static void main(String[] args) throws IOException {
+        Solver.processDay("day04.txt", Day04::processPart1);
+    }
+}
