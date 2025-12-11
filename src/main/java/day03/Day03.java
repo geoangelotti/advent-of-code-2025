01a128e1aae5718bde29da90e91142d9d683ebe9
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-11 22:44:25 +0100

    feat: day 3 part 2 solution
    - not working

--- src/main/java/day03/Day03.java
+++ src/main/java/day03/Day03.java
@@ -32,7 +32,42 @@ public class Day03 {
         return batteries.stream().reduce(0, Integer::sum);
     }
 
+    private static int smallestDigitIndex(String line) {
+        var digits = Arrays.stream(line.split("")).map(Integer::parseInt).toList();
+        int smallestIndex = 0;
+        int smallest = digits.getFirst();
+        for (int i = 1; i < digits.size(); i++) {
+            if (digits.get(i) < smallest) {
+                smallest = digits.get(i);
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    private static long candidateFrom(int number, int smallestDigit, long largest ) {
+        String largestStr = String.valueOf(largest);
+        String candidateStr = largestStr.substring(0, smallestDigit) + largestStr.substring(smallestDigit+1) + number;
+        return Long.parseLong(candidateStr);
+    }
+
     public static Long processPart2(String input) {
-        return null;
+        var lines = parseInput(input);
+        var batteries = new ArrayList<Long>();
+        for (String line : lines) {
+            var digits = line.split("");
+            long largest = Long.parseLong(line.substring(0,12));
+            int smallestIndex = smallestDigitIndex(line);
+            for (int i = 12; i < digits.length; i++) {
+                long candidate = candidateFrom(Integer.parseInt(digits[i]), smallestIndex, largest);
+                IO.println("Candidate: " + candidate + " largest: " + largest);
+                if (candidate > largest) {
+                    largest = candidate;
+                    smallestIndex = smallestDigitIndex(String.valueOf(largest));
+                }
+            }
+            batteries.add(largest);
+        }
+        return batteries.stream().reduce(0L, Long::sum);
     }
 }
