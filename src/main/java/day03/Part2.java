3b374ee7d8d6882c7b5ea441e02b6f3522f04e6d
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-11 22:41:48 +0100

    feat: day 3 part 2 skeleton
    - function
    - test
    - main

---
+++ src/main/java/day03/Part2.java
@@ -0,0 +1,11 @@
+package day03;
+
+import shared.Solver;
+
+import java.io.IOException;
+
+public class Part2 {
+    public static void main(String[] args) throws IOException {
+        Solver.processDay("day03.txt", Day03::processPart2);
+    }
+}
