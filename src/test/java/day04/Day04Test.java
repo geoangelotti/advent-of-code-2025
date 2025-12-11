972e078231f723a30906d56306e91e39e6e22833
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-12 00:14:15 +0100

    feat: day 4 part 2 skeleton
    - function
    - test
    - main

--- src/test/java/day04/Day04Test.java
+++ src/test/java/day04/Day04Test.java
@@ -25,5 +25,11 @@ class Day04Test {
         var result = Day04.processPart1(INPUT);
         assertEquals(Integer.valueOf(13), result);
     }
+
+    @Test
+    void testProcessPart2() {
+        var result = Day04.processPart2(INPUT);
+        assertEquals(Integer.valueOf(43), result);
+    }
 }
 
