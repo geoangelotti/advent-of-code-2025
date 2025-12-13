705b3627e6ee36405b7d41f730cb1068284c085e
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-14 00:33:10 +0100

    feat: day 5 part 1 skeleton
    - class
    - main
    - package
    - test

---
+++ src/main/java/day05/Day05.java
@@ -0,0 +1,7 @@
+package day05;
+
+public class Day05 {
+    public static Integer processPart1(String input) {
+        return null;
+    }
+}
---
+++ src/test/java/day05/Day05Test.java
@@ -0,0 +1,30 @@
+package day05;
+
+import day04.Day04;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day05Test {
+
+    private static final String INPUT = """
+.3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+""";
+
+    @Test
+    void testProcessPart1() {
+        var result = Day05.processPart1(INPUT);
+        assertEquals(Integer.valueOf(3), result);
+    }
+}
+
