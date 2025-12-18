21615e7d66dbf303ffd6769a510a28d760ee3552
Geo Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-18 01:55:12 +0100

    feat: process input

--- src/main/java/day06/Day06.java
+++ src/main/java/day06/Day06.java
@@ -1,6 +1,38 @@
 package day06;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+
 public class Day06 {
+    private static final BinaryOperator<Long> multiply = (x, y) -> x * y;
+
+    private static List<Pair<ArrayList<Long>, String>>parseInput(String input) {
+        ArrayList<ArrayList<Long>> result = new ArrayList<>();
+        var lines = input.split("\n");
+        var lastLine = lines[lines.length - 1].split(" +");
+        Arrays.stream(lastLine).forEach(column -> {
+            result.add(new ArrayList<>());
+        });
+        for (int i = 0; i < lines.length-1; i++) {
+            var columns = lines[i].split(" +");
+            for (int j = 0; j < columns.length; j++) {
+                var column = result.get(j);
+                column.add(Long.parseLong(columns[j]));
+                result.set(j, column);
+            }
+        }
+        var parsed = new ArrayList<Pair<ArrayList<Long>, String>>();
+        for (int j = 0; j < lastLine.length; j++) {
+            parsed.add(Pair.of(result.get(j), lastLine[j]));
+        }
+        return parsed;
+    };
+
     public static Long processPart1(String input) {
         return null;
     }
