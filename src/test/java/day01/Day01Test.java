df744c4cb12a0818c2d99eb23d8cb5218e276e08
Geo Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-20 01:11:45 +0100

    fix: remove unused imports on tests

--- src/test/java/day01/Day01Test.java
+++ src/test/java/day01/Day01Test.java
@@ -1,6 +1,7 @@
 package day01;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day01Test {
--- src/test/java/day04/Day04Test.java
+++ src/test/java/day04/Day04Test.java
@@ -1,6 +1,5 @@
 package day04;
 
-import day03.Day03;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
