d90a9115420f52ea8bd9fa4e52c8da0b87a127bd
Geo Angelopoulos <geoangelotti@users.noreply.github.com>
2025-12-20 01:04:44 +0100

    test: cleanup test file

--- src/test/java/day06/Day06Test.java
+++ src/test/java/day06/Day06Test.java
@@ -1,6 +1,5 @@
 package day06;
 
-import day05.Day05;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,8 +8,8 @@ class Day06Test {
 
     private static final String INPUT = """
 123 328  51 64
-45 64  387 23
-6 98  215 314
+ 45 64  387 23
+  6 98  215 314
 *   +   *   +
 """;
 
