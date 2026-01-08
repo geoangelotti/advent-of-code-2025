99076493f476abb86e982936bc22cc506259c7bd
Geo Angelopoulos <geoangelotti@users.noreply.github.com>
2026-01-08 23:52:08 +0100

    feat: use path traverser on part 1 solution

--- src/main/java/day07/Day07.java
+++ src/main/java/day07/Day07.java
@@ -37,40 +37,13 @@ public class Day07 {
         return Pair.of(start, manifolds);
     }
 
-    public static void traverse(
-            Pair<Integer, Integer> start,
-            Set<Pair<Integer, Integer>> touchedManifolds,
-            Set<Pair<Integer, Integer>> manifolds,
-            Set<Pair<Integer, Integer>> seen,
-            Integer maxHeight,
-            Integer maxWidth
-    ) {
-        var current = start;
-        while (current.getLeft() < maxWidth && current.getRight() < maxHeight && current.getLeft() >= 0) {
-            var next = Pair.of(current.getLeft(), current.getRight() + 1);
-            if (seen.contains(next)) {
-                break;
-            }
-            seen.add(next);
-            if (manifolds.contains(next)) {
-                touchedManifolds.add(next);
-                traverse(Pair.of(
-                        next.getLeft() - 1, next.getRight()), touchedManifolds, manifolds, seen, maxHeight, maxWidth);
-                traverse(Pair.of(
-                        next.getLeft() + 1, next.getRight()), touchedManifolds, manifolds, seen, maxHeight, maxWidth);
-                break;
-            }
-            current = next;
-        }
-    }
-
     public static Integer processPart1(String input) {
         var grid = parseInput(input);
         var startAndManifolds = getStartAndManifolds(grid);
         var start = startAndManifolds.getLeft();
         var manifolds = startAndManifolds.getRight();
-        var touchedManifolds = new HashSet<Pair<Integer, Integer>>();
-        traverse(start, touchedManifolds, manifolds, new HashSet<>(), grid.size(), grid.size());
-        return touchedManifolds.size();
+        var traverser = new PathTraverser(grid.size(), grid.size(), manifolds);
+        traverser.traverse(start);
+        return traverser.getTouchedManifoldsCount();
     }
 }
